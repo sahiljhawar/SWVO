b13183016b3b37e1084211897716119baded372c
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-02-23 13:29:03 +0100

    added argument_parser for requested date

--- plotting/wp3/kp/plot_kp.py
+++ plotting/wp3/kp/plot_kp.py
@@ -6,6 +6,7 @@ matplotlib.use('Agg')
 
 import os
 import sys
+import argparse
 
 sys.path.append("../../../io/wp3")
 from read_kp import KPReader
@@ -30,38 +31,38 @@ def add_bar_color(data, key):
 
 
 def add_subplot(ax, data, title=None, rotation=0, title_font=9, xlabel_fontsize=14,
-                ylabel_fontsize=20, ylim=(-0.1, 9.1), ylabel=r"$K_{p}$", cadance=3):
+                ylabel_fontsize=20, ylim=(-0.1, 9.1), ylabel=r"$K_{p}$", cadence=3):
     # PLOT
-    barcolors = add_bar_color(data, list(data.keys())[0])
-    data.plot(kind="bar", ax=ax, edgecolor=['k'] * len(data), color=[barcolors],
+    bar_colors = add_bar_color(data, list(data.keys())[0])
+    data.plot(kind="bar", ax=ax, edgecolor=['k'] * len(data), color=[bar_colors],
               align="edge", width=0.9, legend=False)
     # TITLE
     plt.title(title, fontsize=title_font)
 
     # Y-AXIS
     ax.set_ylim(ylim)
-    ylabels = [i for i in range(10) if i % 2 == 0]
-    ax.set_yticks(ylabels)
+    y_labels = [i for i in range(10) if i % 2 == 0]
+    ax.set_yticks(y_labels)
     ax.tick_params(axis="y", labelsize=ylabel_fontsize, direction='in')
     ax.set_ylabel(ylabel, fontsize=ylabel_fontsize, rotation=90, labelpad=15)
 
     n_points = len(data)
     if n_points > 12:
-        cadance *= 2
+        cadence *= 2
 
     # X-AXIS
     def map_dates(x):
-        if x.hour % cadance != 0:
+        if x.hour % cadence != 0:
             return ""
-        if (x.hour % cadance == 0) and (x.minute == 0):
+        if (x.hour % cadence == 0) and (x.minute == 0):
             return x.strftime("%H:%M\n%d %b")
         else:
             return x.strftime("%H:%M")
 
     ax.set_xlabel("Time (UTC)", fontsize=15, labelpad=10)
 
-    xlabels = list(data.index.map(lambda x: map_dates(x)))
-    ax.set_xticklabels(labels=xlabels, rotation=rotation, fontsize=xlabel_fontsize)
+    x_labels = list(data.index.map(lambda x: map_dates(x)))
+    ax.set_xticklabels(labels=x_labels, rotation=rotation, fontsize=xlabel_fontsize)
 
     # GRID
     ax.grid(True, axis='y', linestyle='dashed')
@@ -71,7 +72,7 @@ def plot_forecast(model_data):
     _ = plt.figure(figsize=(15, 8))
     ax = plt.subplot(1, 1, 1)
     add_subplot(ax, data=model_data[["kp"]], title=None,
-                cadance=(model_data.index[1] - model_data.index[0]).seconds // 3600)
+                cadence=(model_data.index[1] - model_data.index[0]).seconds // 3600)
 
     red_patch = mpatches.Patch(color='red', label=r'$K_{p}$ > 4')
     yellow_patch = mpatches.Patch(color=[204 / 255.0, 204 / 255.0, 0.0, 1.0], label=r'$K_{p}$ = 4')
@@ -85,24 +86,30 @@ def plot_forecast(model_data):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-date', action="store", default=None, type=str,
+                        help="Requested date to plot in the format %YYYY-%mm-%dd")
+    args = parser.parse_args()
+    plotting_date = args.date
+
     reader = KPReader()
 
-    data_niemegk, date = reader.read("niemegk")
+    data_niemegk, date = reader.read("niemegk", plotting_date)
     plot_forecast(data_niemegk)
     plt.savefig(os.path.join(RESULTS_PATH, "Niemegk_LAST.png"))
     plt.savefig(os.path.join(RESULTS_PATH, "Niemegk_{}.png".format(date.strftime("%Y%m%d"))))
 
-    data_swpc, date = reader.read("swpc")
+    data_swpc, date = reader.read("swpc", plotting_date)
     plot_forecast(data_swpc)
     plt.savefig(os.path.join(RESULTS_PATH, "SWPC_LAST.png"))
     plt.savefig(os.path.join(RESULTS_PATH, "SWPC_{}.png".format(date.strftime("%Y%m%d"))))
 
-    data_l1, date = reader.read("l1")
+    data_l1, date = reader.read("l1", plotting_date)
     plot_forecast(data_l1)
     plt.savefig(os.path.join(RESULTS_PATH, "L1_LAST.png"))
     plt.savefig(os.path.join(RESULTS_PATH, "L1_{}.png".format(date.strftime("%Y%m%d"))))
 
-    data_swift, date = reader.read("swift")
+    data_swift, date = reader.read("swift", plotting_date)
     plot_forecast(data_swift)
     plt.savefig(os.path.join(RESULTS_PATH, "SWIFTKP_LAST.png"))
     plt.savefig(os.path.join(RESULTS_PATH, "SWIFTKP_{}.png".format(date.strftime("%Y%m%d"))))
