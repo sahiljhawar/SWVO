59cffcdac2fd0e6fb23d6a5c6dfed1d9ee4f8f3d
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-02-23 13:16:30 +0100

    first version kp reader and plotting

---
+++ io/wp3/read_kp.py
@@ -0,0 +1,75 @@
+from ..base_file_reader import BaseReader
+import datetime as dt
+import glob
+import pandas as pd
+import os
+import logging
+
+
+class KPReader(BaseReader):
+
+    def __init__(self):
+        super().__init__()
+        self.data_folder = "/PAGER/WP3/data/outputs/SWIFT/"
+
+    @staticmethod
+    def _read_single_file(folder, requested_date=None):
+        start_date = dt.datetime(1900, 1, 1)
+        last_file = None
+
+        if requested_date is None:
+            for file in glob.glob(folder):
+                date = file.split("/")[-1]
+                date = date.split(".")[0]
+                date = dt.datetime.strptime(date.split("_")[-1], "%Y%m%d")
+
+                if date > start_date:
+                    last_file = file
+                    start_date = date
+        else:
+            for file in glob.glob(folder):
+                date = file.split("/")[-1]
+                date = date.split(".")[0]
+                date = dt.datetime.strptime(date.split("_")[-1], "%Y%m%d")
+
+                if date == requested_date:
+                    last_file = file
+                    start_date = date
+                    break
+
+        try:
+            df = pd.read_csv(last_file, names=["t", "kp"])
+            df["t"] = pd.to_datetime(df["t"])
+            df.index = df["t"]
+            df["index"] = ["kp"] * len(df)
+            return df, start_date
+        except FileNotFoundError:
+            logging.error("File not found in folder {}...".format(folder))
+            return None, None
+        except ValueError:
+            logging.error("No file found for requested date {}".format(requested_date))
+            return None, None
+
+    def read(self, source, requested_date=None):
+        """
+        Reads one of the available PAGER Kp forecast products.
+
+        :param source: The source of Kp product requested. Choose among "niemegk", "swift", "swpc" and "l1"
+        :type source: str
+        :param requested_date:
+        :type requested_date: datetime.datetime or None
+        :return:
+        """
+
+        if source == "niemegk":
+            return self._read_single_file(os.path.join(self.data_folder, "NIEMEGK/*"), requested_date)
+        elif source == "swpc":
+            return self._read_single_file(os.path.join(self.data_folder, "SWPC/*"), requested_date)
+        elif source == "l1":
+            return self._read_single_file(os.path.join(self.data_folder, "RBM9/*"), requested_date)
+        elif source == "swift":
+            return self._read_single_file(os.path.join(self.data_folder, "SWIFT/*"), requested_date)
+        else:
+            msg = "Source {} requested for reading Kp not available...".format(source)
+            logging.error(msg)
+            raise RuntimeError(msg)
---
+++ plotting/wp3/kp/plot_kp.py
@@ -0,0 +1,108 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib
+
+matplotlib.use('Agg')
+
+import os
+import sys
+
+sys.path.append("../../../io/wp3")
+from read_kp import KPReader
+
+DATA_PATH = "/PAGER/WP3/data/outputs/"
+RESULTS_PATH = "/PAGER/WP3/data/figures/"
+
+
+def add_bar_color(data, key):
+    color = []
+    for i in range(len(data)):
+        if data[key][i] < 4:
+            color.append('g')
+        elif data[key][i] == 4:
+            color.append([204 / 255.0, 204 / 255.0, 0.0, 1.0])
+            # color.append('y')
+        elif data[key][i] > 4:
+            color.append('r')
+        # elif data[key][i] >= 9.5:
+        #    color.append([0.0, 0.0, 0.0, 0.1])
+    return color
+
+
+def add_subplot(ax, data, title=None, rotation=0, title_font=9, xlabel_fontsize=14,
+                ylabel_fontsize=20, ylim=(-0.1, 9.1), ylabel=r"$K_{p}$", cadance=3):
+    # PLOT
+    barcolors = add_bar_color(data, list(data.keys())[0])
+    data.plot(kind="bar", ax=ax, edgecolor=['k'] * len(data), color=[barcolors],
+              align="edge", width=0.9, legend=False)
+    # TITLE
+    plt.title(title, fontsize=title_font)
+
+    # Y-AXIS
+    ax.set_ylim(ylim)
+    ylabels = [i for i in range(10) if i % 2 == 0]
+    ax.set_yticks(ylabels)
+    ax.tick_params(axis="y", labelsize=ylabel_fontsize, direction='in')
+    ax.set_ylabel(ylabel, fontsize=ylabel_fontsize, rotation=90, labelpad=15)
+
+    n_points = len(data)
+    if n_points > 12:
+        cadance *= 2
+
+    # X-AXIS
+    def map_dates(x):
+        if x.hour % cadance != 0:
+            return ""
+        if (x.hour % cadance == 0) and (x.minute == 0):
+            return x.strftime("%H:%M\n%d %b")
+        else:
+            return x.strftime("%H:%M")
+
+    ax.set_xlabel("Time (UTC)", fontsize=15, labelpad=10)
+
+    xlabels = list(data.index.map(lambda x: map_dates(x)))
+    ax.set_xticklabels(labels=xlabels, rotation=rotation, fontsize=xlabel_fontsize)
+
+    # GRID
+    ax.grid(True, axis='y', linestyle='dashed')
+
+
+def plot_forecast(model_data):
+    _ = plt.figure(figsize=(15, 8))
+    ax = plt.subplot(1, 1, 1)
+    add_subplot(ax, data=model_data[["kp"]], title=None,
+                cadance=(model_data.index[1] - model_data.index[0]).seconds // 3600)
+
+    red_patch = mpatches.Patch(color='red', label=r'$K_{p}$ > 4')
+    yellow_patch = mpatches.Patch(color=[204 / 255.0, 204 / 255.0, 0.0, 1.0], label=r'$K_{p}$ = 4')
+    green_patch = mpatches.Patch(color='green', label=r'$K_{p}$ < 4')
+    transparent_patch = mpatches.Patch(color=[0, 0, 0, 0.1], label='Data not available')
+    plt.legend(bbox_to_anchor=(0., 1., 0.84, .275), handles=[green_patch, yellow_patch, red_patch, transparent_patch],
+               ncol=4, fontsize="x-large", shadow=True)
+
+    plt.subplots_adjust(left=None, bottom=0.3, right=None, top=0.7,
+                        wspace=None, hspace=0.6)
+
+
+if __name__ == "__main__":
+    reader = KPReader()
+
+    data_niemegk, date = reader.read("niemegk")
+    plot_forecast(data_niemegk)
+    plt.savefig(os.path.join(RESULTS_PATH, "Niemegk_LAST.png"))
+    plt.savefig(os.path.join(RESULTS_PATH, "Niemegk_{}.png".format(date.strftime("%Y%m%d"))))
+
+    data_swpc, date = reader.read("swpc")
+    plot_forecast(data_swpc)
+    plt.savefig(os.path.join(RESULTS_PATH, "SWPC_LAST.png"))
+    plt.savefig(os.path.join(RESULTS_PATH, "SWPC_{}.png".format(date.strftime("%Y%m%d"))))
+
+    data_l1, date = reader.read("l1")
+    plot_forecast(data_l1)
+    plt.savefig(os.path.join(RESULTS_PATH, "L1_LAST.png"))
+    plt.savefig(os.path.join(RESULTS_PATH, "L1_{}.png".format(date.strftime("%Y%m%d"))))
+
+    data_swift, date = reader.read("swift")
+    plot_forecast(data_swift)
+    plt.savefig(os.path.join(RESULTS_PATH, "SWIFTKP_LAST.png"))
+    plt.savefig(os.path.join(RESULTS_PATH, "SWIFTKP_{}.png".format(date.strftime("%Y%m%d"))))
