41d88ac30a7e0ef82bede2c4449ea00bdb06611c
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-02-25 15:39:39 +0100

    working on docker file for plotting all data

---
+++ docker/plot_all_docker.sh
@@ -0,0 +1,6 @@
+PYTHON=/opt/conda/envs/pager/bin/python
+INPUT_DIR=/
+OUTPUT_DIR=/
+LOG_DIR=/
+
+$PYTHON /data_management/scripts/plot/wp3/plot_all_kp.py -input $INPUT_DIR -output $OUTPUT_DIR -logdir $LOG_DIR
--- scripts/plot/wp3/plot_all_kp.py
+++ scripts/plot/wp3/plot_all_kp.py
@@ -11,7 +11,6 @@ from read_kp import KPReader
 sys.path.append("/PAGER/WP8/data_management/plotting/wp3/kp/")
 from plot_kp import PlotKpOutput
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-date', action="store", default=None, type=str,
@@ -20,8 +19,14 @@ if __name__ == "__main__":
                         help="Path to a folder where to store the produced figures")
     parser.add_argument('-input', action="store", default="/PAGER/WP3/data/outputs/", type=str,
                         help="Path to a folder where the data to plot is stored...(be more precise)")
+    parser.add_argument('-logdir', action="store", default=None, type=str,
+                        help="Log directory if logging is to be enabled.")
 
     args = parser.parse_args()
+
+    if args.logdir is not None:
+        log_file = "wp3_plot_all_kp_{}.log".format(args.date)
+        logging.basicConfig(filename=os.path.join(args.logdir, log_file), level=logging.INFO)
 
     RESULTS_PATH = args.output
     DATA_PATH = args.input
@@ -33,42 +38,58 @@ if __name__ == "__main__":
 
     date_now = dt.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
     try:
+        logging.info("Reading Niemegk Kp nowcast data file...")
         data_niemegk, date = reader.read("niemegk", plotting_date)
+        logging.info("...Complete!!")
+        logging.info("Plotting and saving Niemegk Kp nowcast data...")
         plotter.plot_output(data_niemegk)
         if plotting_date >= date_now:
             plt.savefig(os.path.join(RESULTS_PATH, "Niemegk_LAST.png"))
         plt.savefig(os.path.join(RESULTS_PATH, "Niemegk_{}.png".format(date.strftime("%Y%m%d"))))
+        logging.info("...Complete!!")
     except TypeError:
         logging.error(
             "Data for Niemegk nowcast for date {} not found...impossible to produce data plot...".format(plotting_date))
 
     try:
+        logging.info("Reading SWPC Kp forecast data file...")
         data_swpc, date = reader.read("swpc", plotting_date)
+        logging.info("...Complete!!")
+        logging.info("Plotting and saving SWPC Kp nowcast data...")
         plotter.plot_output(data_swpc)
         if plotting_date >= date_now:
             plt.savefig(os.path.join(RESULTS_PATH, "SWPC_LAST.png"))
         plt.savefig(os.path.join(RESULTS_PATH, "SWPC_{}.png".format(date.strftime("%Y%m%d"))))
+        logging.info("...Complete!!")
     except TypeError:
         logging.error(
             "Data for SWPC forecast for date {} not found...impossible to produce data plot...".format(plotting_date))
 
     try:
+        logging.info("Reading L1 Kp forecast data file...")
         data_l1, date = reader.read("l1", plotting_date)
+        logging.info("...Complete!!")
+        logging.info("Plotting and saving L1 Kp forecast data...")
         plotter.plot_output(data_l1)
-        if plotting_date >= date_now: 
-            plt.savefig(os.path.join(RESULTS_PATH, "L1_LAST.png")) 
+        if plotting_date >= date_now:
+            plt.savefig(os.path.join(RESULTS_PATH, "L1_LAST.png"))
         plt.savefig(os.path.join(RESULTS_PATH, "L1_{}.png".format(date.strftime("%Y%m%d"))))
+        logging.info("...Complete!!")
     except TypeError:
         logging.error(
             "Data for L1 Kp forecast for date {} not found...impossible to produce data plot...".format(plotting_date))
 
-
     try:
+        logging.info("Reading SWIFT Kp forecast data file...")
         data_swift, date = reader.read("swift", plotting_date)
+        logging.info("...Complete!!")
+        logging.info("Plotting and saving SWIFT Kp forecast data...")
         plotter.plot_output(data_swift)
-        if plotting_date >= date_now: 
-            plt.savefig(os.path.join(RESULTS_PATH, "SWIFT_LAST.png")) 
+        if plotting_date >= date_now:
+            plt.savefig(os.path.join(RESULTS_PATH, "SWIFT_LAST.png"))
         plt.savefig(os.path.join(RESULTS_PATH, "SWIFT_{}.png".format(date.strftime("%Y%m%d"))))
+        logging.info("...Complete!!")
     except TypeError:
         logging.error(
-            "Data for SWIFT Kp forecast for date {} not found...impossible to produce data plot...".format(plotting_date))
+            "Data for SWIFT Kp forecast for date {} not found...impossible to produce data plot...".format(
+                plotting_date))
