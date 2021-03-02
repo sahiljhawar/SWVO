71d1080ffeb521527e429d192a3e7ed1b7cf837f
Stefano Bianco <bianco@gfz-potsdam.de>
2021-03-02 19:59:43 +0100

    removed unsued method

--- plotting/wp3/plot_gfz_plasma.py
+++ plotting/wp3/plot_gfz_plasma.py
@@ -6,8 +6,8 @@ import numpy as np
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
+
+import matplotlib.ticker as mticker
 
 from datetime import datetime
 
@@ -26,69 +26,40 @@ class PlasmaspherePlot(BasePlot):
         :param ax:
         :return:
         """
+        self._get_ax().fill_between(np.linspace(-np.pi / 2, np.pi / 2, 100),
+                                    0, 1, alpha=1, color='k')
 
-        Ls = np.linspace(0, 1, 100)
-        angles = np.linspace(-math.pi / 2, math.pi / 2, 600)
+    def _set_axes_ticks_labels(self):
 
-        angle_matrix, L_matrix = np.meshgrid(angles, Ls)
-
-        angle_column = angle_matrix.flatten('F').reshape(-1, 1)
-        L_column = L_matrix.flatten('F').reshape(-1, 1)
-
-        self._get_ax().scatter(angle_column, L_column, s=0.1, c="black")
-
-    def _draw_earth_day_side(self):
-        """
-        It draws the day side of the earth
-
-        :param ax:
-        :return:
-        """
-
-        Ls = np.linspace(0, 1, 20)
-        angles = np.linspace(math.pi / 2, 3 * math.pi / 2, 150)
-
-        angle_matrix, L_matrix = np.meshgrid(angles, Ls)
-
-        angle_column = angle_matrix.flatten('F').reshape(-1, 1)
-        L_column = L_matrix.flatten('F').reshape(-1, 1)
-
-        self._get_ax().scatter(angle_column, L_column, s=0.1, c="w")
-
-    def _set_style(self):
-
-        # set stile
-        self._get_ax().set_ylim(0, 6)
+        ticks_loc = [x for x in np.linspace(0, 2 * math.pi, 8, endpoint=False)]
+        self._get_ax().xaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
         self._get_ax().set_xticklabels(
-            [xlabel for xlabel in np.arange(0, 24, 3)],
-            fontdict={'fontweight': "roman", 'fontsize': 18}
+            labels=[xlabel for xlabel in np.arange(0, 24, 3)]
         )
+
+        print([xlabel for xlabel in np.arange(0, 24, 3)])
+
+        ticks_loc = [1, 2, 3, 4, 5, 6]
+        self._get_ax().yaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
+        self._get_ax().set_ylim(0, 6.5)
         self._get_ax().set_yticklabels(
-            [1, 2, 3, 4, 5, 6],
-            fontdict={'fontweight': "roman", 'fontsize': 16})
-        self._get_ax().grid(c='w', lw=1.5, ls='-', visible=True)
+            labels=ticks_loc
+        )
+        self._get_ax().grid(c='grey', lw=0.5, ls='-', visible=True)
+
 
     def _plot_plasma_density(self,
                              angle_values,
                              l_values,
-                             density_values):
+                             density_values
+                             ):
         self._get_ax().scatter(angle_values,
-                                   l_values,
-                                   c=density_values,
-                                   s=l_values,
-                                   cmap=self._get_colour_map(),
-                                   alpha=0.4
-                                   )
-
-        #vmin, vmax = c.get_clim()
-
-        #return vmin, vmax
-
-    def _set_title_coordinates(self, title_coordinates):
-        self.title_coordinates = title_coordinates
-
-    def _get_title_coordinates(self):
-        return self.title_coordinates
+                               l_values,
+                               c=density_values,
+                               s=l_values ** 2,
+                               cmap=self._get_colour_map(),
+                               alpha=0.4
+                               )
 
     def _set_colour_bar_axes(self, axes):
         self.colour_bar_axes = axes
@@ -98,18 +69,38 @@ class PlasmaspherePlot(BasePlot):
 
     def _add_colour_bar(self):
 
-        self._set_colour_bar_axes(self._get_figure().add_axes([0.9, 0.2, 0.04, 0.6]))
+
+
+        # self._set_colour_bar_axes(self._get_figure().add_axes([0.87, 0.2, 0.04, 0.6],
+        #                                                       in_layout=True))
 
         colour_bar_normalization = mpl.colors.Normalize(vmin=0,
-                                                        vmax=3.9
+                                                        vmax=3.99
                                                         )
-        cb = mpl.colorbar.ColorbarBase(self._get_colour_bar_axes(),
-                                       cmap=self._get_colour_map(),
-                                       norm=colour_bar_normalization)
-        cb.set_label('log10(n\u2091), 1/cm\u00b3 ')
 
-        self._get_colour_bar_axes().tick_params(direction="in")
-        self._get_colour_bar_axes().tick_params(axis='both', which='major', labelsize=18)
+        color_bar = plt.colorbar(mpl.cm.ScalarMappable(
+            norm=colour_bar_normalization,
+            cmap=self._get_colour_map()
+        ),
+            ax=self._get_ax(),
+            use_gridspec=True,
+            shrink=0.5,
+            pad=0.15
+        )
+        color_bar.set_label('$log_{10}(n_e)$')
+        # mpl.colorbar.ColorbarBase(self._get_colour_bar_axes(),
+        #                           cmap=self._get_colour_map(),
+        #                           norm=colour_bar_normalization,
+        #                           ticklocation="right")
+        #
+        # self._get_colour_bar_axes().set_ylabel('$log_{10}(n_e)$',
+        #                                        size=16)
+        #
+        # self._get_colour_bar_axes().tick_params(axis='both', which='major',
+        #                                         labelsize=18, direction="in")
+
+
+
 
     def _set_colour_map(self):
         self.colour_map = mpl.colors.ListedColormap(np.load('./my_cmap.npy'))
@@ -147,44 +138,39 @@ class PlasmaspherePlot(BasePlot):
             raise ValueError("densities must not contain NaNs")
 
     def plot(self,  l_values, mlt_values, density_values, date,
-             fig_size=(7, 6.5), title_coordinates=(math.pi / 2 + 0.45, 8)):
+             fig_size=(7, 6.5)):
 
-        self._check_inputs(density_values)
 
         self._set_date(date)
+        fig, ax = plt.subplots(subplot_kw=dict(projection='polar'),
+                               figsize=fig_size)
+        self._set_figure(fig)
+        self._set_ax(ax)
 
-        self._set_title_coordinates(title_coordinates)
+        #self._check_inputs(density_values)
 
         self._set_colour_map()
 
-        self._set_figure(plt.figure(figsize=fig_size))
-
-        self._set_ax(self._get_figure().add_subplot(1, 1, 1, projection='polar'))
-
         angle_values = PlasmaspherePlot._mlt_to_angle(mlt_values)
-
         self._plot_plasma_density(
             angle_values,
             l_values,
-            density_values
+            density_values,
         )
 
         self._draw_earth_night_side()
-        self._draw_earth_day_side()
 
-        self._set_style()
+        self._set_axes_ticks_labels()
 
-        self._get_ax().text(
-            x=self._get_title_coordinates()[0],
-            y=self._get_title_coordinates()[1],
-            s="{}".format(self._get_date().strftime("%Y-%m-%d, %H:%M")),
-            fontdict={'fontsize': 20, 'fontweight': "bold"})
+        self._get_ax().set_title(
+            "{}".format(self._get_date().strftime("%Y-%m-%d, %H:%M")),
+        fontdict={'fontweight': 'bold'})
 
         self._add_colour_bar()
 
-        self._get_figure().subplots_adjust(left=0.05, right=0.85)
+        self._get_figure().subplots_adjust(left=0.07, right=0.8)
+        plt.tight_layout()
 
     def save(self, path):
         plt.savefig(path)
         plt.close()
-
