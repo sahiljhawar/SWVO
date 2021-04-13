c6477e78004ce40ee54fd48a26b088d737187df1
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-04-13 12:15:30 +0200

    added kp to output checker

--- scripts/check_file_generation/check_all_outputs.py
+++ scripts/check_file_generation/check_all_outputs.py
@@ -3,37 +3,53 @@ import logging
 import os
 import datetime as dt
 import sys
+import inspect
 
-sys.path.append("/PAGER/WP8/data_management/")
+LOCAL_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+sys.path.append(os.path.join(LOCAL_PATH, "../../"))
 
 from data_management.check.wp2.check_swift import SwiftCheck
-# from data_management.check.wp3.
+from data_management.check.wp3.check_kp import KpDataCheck
+
+
+def wp2_check_swift(date):
+    checker = SwiftCheck()
+    checker.run_check(date)
+
+
+def wp3_check_kp(date, product, model=None):
+    checker = KpDataCheck()
+    checker.run_check(product, model, date)
 
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-date', action="store", default=None, type=str,
-                        help="Requested date to plot in the format %YYYY-%mm-%dd")
+                        help="Requested date to plot in the format %YYYY%mm%ddT%HH")
     parser.add_argument('-logdir', action="store", default=None, type=str,
                         help="Log directory if logging is to be enabled.")
 
     args = parser.parse_args()
 
+    if args.date is not None:
+        check_date = args.date + "0000"
+    else:
+        check_date = dt.datetime.utcnow().replace(minute=0, second=0, microsecond=0).strftime("%Y%m%dT%H%M%S")
+
     if args.logdir is not None:
-        if args.date is not None:
-            log_file = "wp3_plot_all_kp_{}.log".format(args.date)
-        else:
-            log_date = dt.datetime.utcnow().replace(hour=0, minute=0, second=0,
-                                                    microsecond=0).strptime(args.date, "%Y-%m-%d")
-            log_file = "wp3_plot_all_kp_{}.log".format(log_date)
+        log_file = "wp8_output_checker_{}.log".format(check_date)
         logging.basicConfig(filename=os.path.join(args.logdir, log_file), level=logging.INFO)
 
-    if args.date is not None:
-        check_date = dt.datetime.strptime(args.date, "%Y-%m-%d")
-    else:
-        check_date = None
     # WP2
     # SWIFT
-    
-    checker = SwiftCheck()
-    checker.run_check(check_date)
+    wp2_check_swift(check_date)
+
+    # WP3
+    wp3_check_kp(check_date, "swpc")
+    wp3_check_kp(check_date, "niemegk")
+    wp3_check_kp(check_date, "swift")
+    wp3_check_kp(check_date, "l1", model="KP-FULL-SW-PAGER")
+    wp3_check_kp(check_date, "l1", model="HP60-FULL-SW-SWAMI-PAGER")
+
+
