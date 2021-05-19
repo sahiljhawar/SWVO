0dddd725e05cf70a5819dd3a6561dcf4b36c41be
Stefano Bianco <bianco@gfz-potsdam.de>
2021-05-19 13:39:08 +0200

    fixed source name

--- scripts/plot/wp3/plot_all_plasma.py
+++ scripts/plot/wp3/plot_all_plasma.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     video_name = "gfz_plasma_video_{}.mp4".format(plotting_date.strftime("%Y%m%dT%H%M"))
     try:
         logging.info("Reading GFZ Plasmasphere forecast data file...")
-        data = reader.read("gfz_potsdam", plotting_date)
+        data = reader.read("gfz_plasma", plotting_date)
         logging.info("...Complete!!")
         logging.info("Plotting and saving plasmasphere forecast video...")
         plotter.plot_output(data, RESULTS_PATH, video_name)
