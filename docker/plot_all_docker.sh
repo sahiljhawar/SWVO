2b419238413b72854e02fd667a3a32355987febe
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-06-23 14:08:24 +0200

    Prepared plotting routines to run on docker

--- docker/plot_all_docker.sh
+++
@@ -1,6 +0,0 @@
-PYTHON=/opt/conda/envs/pager/bin/python
-
-mkdir -p /logs
-mkdir -p /results
-
-$PYTHON /data_management/scripts/plot/wp3/plot_all_kp.py -input /inputs/ -output /results -logdir /logs
