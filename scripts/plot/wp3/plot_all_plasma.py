e0df0c50215d2cb274262acb4551eb2f447dd082
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-06-23 10:54:01 +0200

    changed format for date cl parameter

--- scripts/plot/wp3/plot_all_plasma.py
+++ scripts/plot/wp3/plot_all_plasma.py
@@ -13,7 +13,7 @@ from data_management.plotting.wp3.plasmasphere.plasmasphere_plot import Plasmasp
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-date', action="store", default=None, type=str,
-                        help="Requested date to plot in the format %YYYY-%mm-%dd")
+                        help="Requested date to plot in the format %YYYY%mm%dd")
     parser.add_argument('-output', action="store", default="/PAGER/WP3/data/figures/plasmasphere/", type=str,
                         help="Path to a folder where to store the produced figures")
     parser.add_argument('-input', action="store", default="/PAGER/WP3/data/outputs/", type=str,
@@ -28,9 +28,9 @@ if __name__ == "__main__":
         plotting_date = date_now
     else:
         try:
-            plotting_date = dt.datetime.strptime(args.date, "%Y-%m-%d-%H-%M")
+            plotting_date = dt.datetime.strptime(args.date, "%Y%m%d")
         except TypeError:
-            msg = "Provided date {} not in correct format %Y-%m-%d. Aborting...".format(args.date)
+            msg = "Provided date {} not in correct format %Y%m%d. Aborting...".format(args.date)
             logging.error(msg)
             raise RuntimeError(msg)
 
