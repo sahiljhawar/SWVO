91c8946e4e933f91a4a54339cd407c5911791d82
Ruggero <ruggero@gfz-potsdam.de>
2021-06-24 09:21:57 +0200

    fixed requirement file and network host for container

--- docker/check_file_generation/run_file_check.sh
+++ docker/check_file_generation/run_file_check.sh
@@ -7,6 +7,6 @@ TEMP_LOG_DIR=/logs/
 CONTAINER_NAME=wp8_filecheck_container
 LOG_DIR=/PAGER/WP8/logs/
 
-docker run -v $INPUT_DIR:$INPUT_DIR --name $CONTAINER_NAME  $IMAGE /bin/bash -c "$PYTHON /data_management/scripts/check_file_generation/check_all_outputs.py -logdir $TEMP_LOG_DIR -notify 1"
+docker run -v $INPUT_DIR:$INPUT_DIR --network="host" --name $CONTAINER_NAME  $IMAGE /bin/bash -c "$PYTHON /data_management/scripts/check_file_generation/check_all_outputs.py -logdir $TEMP_LOG_DIR -notify 1"
 docker container cp $CONTAINER_NAME:$TEMP_LOG_DIR. $LOG_DIR
 docker rm $CONTAINER_NAME
