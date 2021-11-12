42a66a329223dd9aecda5578fe4e35b368195076
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-11-12 13:51:48 +0100

    Added docker version with plot and date option ,to be tested

--- docker/make_plots.sh
+++ docker/make_plots.sh
@@ -4,6 +4,7 @@ Help() {
 cat << EOF
 
    Usage: make_plots.sh [-I IMAGE] [-d DATA_FOLDER] [-r RECURRENT] [-U USERID] [-G GROUPID] [-P PLOT] [- l LOG_DIR]
+                        [-D DATE]
 
    This script uses a docker container to generate a forecast using swift solar wind data as input. The machine learning
    model is build using the geoforecast library and saved.
@@ -13,10 +14,11 @@ cat << EOF
        -d DATA_FOLDER  Path to base pager data folder, input and output data are taken from there at the moment
        -r RECURRENT    To run it once or with sleeping time continuously. By default it runs once, otherwise set to
                        1 please
-       -P PLOT         If specified, you perform only one specific plot (not working at the moment, all are produced)
+       -P PLOT         If specified, you perform only one specific plot. Now working "swift", "kp", "plasma" only
        -U USERID       The user id which writes files in the bind volume.
        -G GROUPID      The group id to which the written files belong to.
-       -l LOG_DIR      (optional)
+       -l LOG_DIR      (optional, not working properly)
+       -D DATE         Date for which we want to plot the data in the format "YYYYMMDDHH" (e.g. 2021010110)
 
 EOF
 }
@@ -24,7 +26,7 @@ EOF
 RECURRENT=0
 PLOT=
 LOG_DIR=
-while getopts hI:d:r:P:U:G:l: flag
+while getopts hI:d:r:P:U:G:l:D: flag
 do
 	case "${flag}" in
 	        I) IMAGE=${OPTARG};;
@@ -34,6 +36,7 @@ do
 	        U) USERID=${OPTARG};;
 	        G) GROUPID=${OPTARG};;
 	        l) LOG_DIR=${OPTARG};;
+	        D) DATE=${OPTARG};;
 	        h) Help ;;
 	        *) exit;;
     esac
@@ -61,5 +64,34 @@ then
   GROUPID="65534"
 fi
 
+if [ -z "$DATE" ]
+then
+  echo "Date not specified, running real time..."
+else
+
+  if [[ "$DATE" =~ ^[0-2][0-9]{3}[0-1][0-9][0-3][0-9][0-2][0-9]$ ]]
+  then
+    echo
+  else
+    echo "Wrong date format, it should be a valid date in the format %YYYY%MM%DD%HH"
+    exit 1
+  fi
+fi
+
+if [ -z "$PLOT" ]
+then
+  echo "Plotting all products..."
+else
+
+  if [[ "$PLOT" == kp ]] || [[ "$PLOT" == swift ]] || [[ "$PLOT" == plasma ]]
+  then
+    echo "Plotting $PLOT data"
+  else
+    echo "Wrong plotting option, either leave blank or choose among kp, plasma or swift"
+    exit 1
+  fi
+fi
+
 docker run -d --rm -v "$DATA_FOLDER":/PAGER -u=$USERID:$GROUPID --env RECURRENT="$RECURRENT" --env LOG_DIR="$LOG_DIR" \
+       --env DATE="$DATE" --env PLOT="$PLOT" \
        --entrypoint="./plot_entrypoint.sh" "$IMAGE"
--- docker/plot_entrypoint.sh
+++ docker/plot_entrypoint.sh
@@ -7,8 +7,26 @@ else
       pargs=("-logdir" "$LOG_DIR");
 fi
 
-/opt/conda/envs/data_management/bin/python /scripts/plot/wp2/plot_all_swift.py "${pargs[@]}"
+if [ -z "$DATE" ]
+then
+      echo
+else
+      pargs=("-date" "$DATE");
+fi
 
-/opt/conda/envs/data_management/bin/python /scripts/plot/wp3/plot_all_kp.py "${pargs[@]}"
+if [ -z "$PLOT" ]
+then
+      /opt/conda/envs/data_management/bin/python /scripts/plot/wp2/plot_all_swift.py "${pargs[@]}"
+      /opt/conda/envs/data_management/bin/python /scripts/plot/wp3/plot_all_kp.py "${pargs[@]}"
+      /opt/conda/envs/data_management/bin/python /scripts/plot/wp3/plot_all_plasma.py "${pargs[@]}"
+else
+      case $PLOT in
+        swift)
+        /opt/conda/envs/data_management/bin/python /scripts/plot/wp2/plot_all_swift.py "${pargs[@]}" ;;
+        kp)
+        /opt/conda/envs/data_management/bin/python /scripts/plot/wp3/plot_all_kp.py "${pargs[@]}" ;;
+        plasma)
+        /opt/conda/envs/data_management/bin/python /scripts/plot/wp3/plot_all_plasma.py "${pargs[@]}" ;;
+      esac
+fi
 
-/opt/conda/envs/data_management/bin/python /scripts/plot/wp3/plot_all_plasma.py "${pargs[@]}"
