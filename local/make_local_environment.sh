cbcd56fda632d5dc158693fb1c84220200c6e9dd
Ruggero Vasile <ruggero@gfz-potsdam.de>
2021-11-15 09:44:54 +0100

    Added choice of environment name

--- local/make_local_environment.sh
+++ local/make_local_environment.sh
@@ -1,10 +1,38 @@
 #!/usr/bin/env bash
 
-ENV_NAME=data_management
+Help() {
+cat << EOF
+
+   Usage: make_local_environment.sh [-e PYTHON_ENV]
+
+   This script is used to prepare a local conda environment to run checks and plotting routines.
+
+   optional arguments:
+       -e PYTHON_ENV   The name of the python conda environment to create. If not specified the
+                       default "data_management" will be used
+EOF
+}
+
+while getopts he: flag
+do
+	case "${flag}" in
+	        e) PYTHON_ENV=${OPTARG};;
+	        h) Help ;;
+	        *) exit;;
+    esac
+done
+
+
+if [ -z "$PYTHON_ENV" ]
+  then
+    echo "Python environment name not specified using default name data_management "
+    PYTHON_ENV=data_management
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-conda create -n "$ENV_NAME" python=3.8
-source activate "$ENV_NAME" || conda activate "$ENV_NAME"
+conda create -n "$PYTHON_ENV" python=3.8
+source activate "$PYTHON_ENV" || conda activate "$PYTHON_ENV"
 
 cd "$SCRIPT_DIR" || exit
 bash ../install.sh
