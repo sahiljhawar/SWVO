ebcada7af5b704eeb80552071eb5bb60f230a658
Stefano Bianco <bianco@gfz-potsdam.de>
2024-03-07 15:34:08 +0100

    added plotter for hp60 ensemble output

--- data_management/plotting/wp3/hp60/plot_hp60.py
+++ data_management/plotting/wp3/hp60/plot_hp60.py
@@ -1,7 +1,12 @@
-from data_management.plotting.wp3.kp.plot_kp import PlotKpOutput
+from data_management.plotting.wp3.kp.plot_kp import PlotKpOutput, PlotKpEnsembleOutput
 
 
 class PlotHp60(PlotKpOutput):
+
+    def __init__(self):
+        super().__init__()
+
+class PlotHp60Ensemble(PlotKpEnsembleOutput):
 
     def __init__(self):
         super().__init__()
