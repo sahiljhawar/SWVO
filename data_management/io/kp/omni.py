53d5808cb6e57552554fcef136e1bf443b98273b
Sahil Jhawar <jhawar@gfz-potsdam.de>
2024-11-15 16:57:45 +0100

    init method in child class

--- data_management/io/kp/omni.py
+++ data_management/io/kp/omni.py
@@ -9,16 +9,19 @@ from data_management.io.omni import OMNILowRes
 
 class KpOMNI(OMNILowRes):
 
+    def __init__(self, data_dir=None):
+        super().__init__(data_dir=data_dir)
+
     def read(self, start_time: datetime, end_time: datetime, download: bool = False) -> pd.DataFrame:
 
         data_out = super().read(start_time, end_time, download=download)
         df = pd.DataFrame(index=data_out.index)
 
-        df = data_out["kp"]
+        df["kp"] = data_out["kp"]
 
         # we return it just every 3 hours
         df.drop(df[data_out.index.hour % 3 != 0].index, axis=0, inplace=True)
         df.index = df.index.tz_localize("UTC")
         df = df.truncate(before=start_time - timedelta(hours=2.9999), after=end_time + timedelta(hours=2.9999))
 
-        return pd.DataFrame(df)
+        return df
