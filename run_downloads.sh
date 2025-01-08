c70c363e2df57770ac30a2e4f1880603c5f5015f
Stefano Bianco <bianco@gfz-potsdam.de>
2025-01-08 14:42:55 +0100

    set up python environment and path to script setting environment variable as parameters of the script

--- run_downloads.sh
+++ run_downloads.sh
@@ -1,4 +1,42 @@
-#!/bin/bash
-source /home/pager/FLAG_DEV/code/external_data/data_management/venv/bin/activate
-source /home/pager/FLAG_DEV/code/external_data/data_management/set_env_variables.sh
-python3 /home/pager/FLAG_DEV/code/external_data/data_management/download_daily_files.py
+#!/usr/bin/env bash
+
+Help() {
+cat << EOF
+
+   Usage: make_local_environment.sh [-e PYTHON_ENV]
+
+   This script is used to prepare a local conda environment to run checks and plotting routines.
+
+   optional arguments:
+       -e PYTHON_ENV   The name of the python conda environment to create. If not specified the
+                       default "data_management" will be used
+EOF
+}
+
+while getopts he: flag
+do
+	case "${flag}" in
+	        e) PYTHON_ENV=${OPTARG};;
+	        v) VENV_VARIABLES=${OPTARG};;
+	        v) Help ;;
+	        *) exit;;
+    esac
+done
+if [ -z "$PYTHON_ENV" ]
+  then
+    echo "You need to provide an absolute path to a python environment (-e option). Exiting..."
+    exit
+fi
+source $PYTHON_ENV/bin/activate
+
+if [ -z "$VENV_VARIABLES" ]
+  then
+    echo "You need to provide an absolute path and name to the environment variable script (-v option). Exiting..."
+    exit
+fi
+source $VENV_VARIABLE
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "$SCRIPT_DIR" || exit
+
+python3 ./download_daily_files.py
