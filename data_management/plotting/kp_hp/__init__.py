f4b813ddff2f9b423e33dd55de4e8d0dd29dabe1
Sahil Jhawar <jhawar@gfz-potsdam.de>
2025-07-10 13:46:25 +0200

    remove plotting from data_management

--- data_management/plotting/kp_hp/__init__.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
-#
-# SPDX-License-Identifier: Apache-2.0
-
-import data_management.plotting.kp_hp.plot_kp_hp
--- data_management/plotting/swift/__init__.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
-#
-# SPDX-License-Identifier: Apache-2.0
-
-import data_management.plotting.swift.plot_swift
