baf17942335fbed77b6f3ec14ee96f5f26ab9f5d
Sahil Jhawar <jhawar@gfz-potsdam.de>
2025-07-24 19:01:24 +0200

    fix run downloads

--- run_downloads.sh
+++ run_downloads.sh
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-. .venv/bin/activate
+source /PAGER/FLAG/code/external_data/data_management/.venv/bin/activate
 source /PAGER/FLAG/set_env_variables.sh
 python3 /PAGER/FLAG/code/external_data/data_management/download_daily_files.py --logs /PAGER/FLAG/logs/daily_downloads_external_data/
