38661476116b5b23d31d810c2e68d8524fe77f5a
Parvathy S <parvathy@gfz.de>
2026-01-06 11:44:02 +0100

    sym_h addition

--- swvo/io/omni/omni_high_res.py
+++ swvo/io/omni/omni_high_res.py
@@ -129,7 +129,7 @@ class OMNIHighRes:
         start_time: datetime,
         end_time: datetime,
         cadence_min: float = 1,
-        download: bool = False,
+        download: bool = True,
     ) -> pd.DataFrame:
         """
         Read OMNI High Resolution data for the given time range.
@@ -288,6 +288,7 @@ class OMNIHighRes:
             "proton_density": 999.8,
             "temperature": 9999998.0,
             "pdyn": 99.0,
+            "sym-h": 99999.0,
         }
 
         df.columns = maxes.keys()
@@ -351,7 +352,7 @@ class OMNIHighRes:
             "start_date": start.strftime("%Y%m%d"),
             "end_date": end.strftime("%Y%m%d"),
         }
-        common_vars = {"vars": ["13", "14", "17", "18", "21", "25", "26", "27"]}
+        common_vars = {"vars": ["13", "14", "17", "18", "21", "25", "26", "27", "42"]}
         if cadence == 1:
             params = {"res": "min", "spacecraft": "omni_min"}
             payload.update(params)
