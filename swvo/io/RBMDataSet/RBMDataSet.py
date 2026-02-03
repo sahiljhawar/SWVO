fc1fbc390f383274db43d2b9b38e74c59150e9fc
Bernhard Haas <bhaas@gfz.de>
2026-02-03 15:13:58 +0100

    Added identify_orbits function in RBMDataSet.

--- swvo/io/RBMDataSet/RBMDataSet.py
+++ swvo/io/RBMDataSet/RBMDataSet.py
@@ -263,7 +263,7 @@ class RBMDataSet:
         """Returns the MFM enum."""
         return self._mfm
 
-    def update_from_dict(self, source_dict: dict[str, VariableLiteral]) -> RBMDataSet:
+    def update_from_dict(self, source_dict: dict[VariableLiteral, NDArray[np.floating] | list[dt.datetime]]) -> RBMDataSet:
         """Get data from data dictionary and update the object.
 
         Parameters
---
+++ swvo/io/RBMDataSet/identify_orbits.py
@@ -0,0 +1,61 @@
+import typing
+from datetime import datetime
+from typing import Literal, NamedTuple
+
+import numpy as np
+from numpy.typing import NDArray
+from scipy.interpolate import make_splrep  # type: ignore[reportUnknownVariableType]
+from scipy.signal import find_peaks  # type: ignore[reportUnknownVariableType]
+
+from swvo.io.RBMDataSet import RBMDataSet, RBMNcDataSet
+
+class Trajectory(NamedTuple):
+    start: int
+    end: int
+    direction: Literal["inbound", "outbound"]
+
+def _identify_orbits(
+        time: list[datetime],
+        distance: NDArray[np.floating],
+        minimal_distance: int,
+        *,
+        apply_smoothing: bool) -> list[Trajectory]:
+
+    if apply_smoothing:
+        timestamps = [t.timestamp() for t in time]
+        distance = make_splrep(timestamps, distance, s=0)(timestamps)  # type: ignore[reportUnknownVariableType]
+        distance = typing.cast("NDArray[np.floating]", distance)
+
+    peaks, _ = find_peaks(distance, distance=minimal_distance)  # type: ignore[reportUnknownVariableType]
+    troughs, _ = find_peaks(-distance, distance=minimal_distance)  # type: ignore[reportUnknownVariableType]
+    extrema = np.sort(np.concatenate((peaks, troughs)))  # type: ignore[reportUnknownVariableType]
+    extrema = typing.cast("NDArray[np.int32]", extrema)
+
+    diffs = np.diff(distance)
+    in_out_bound_label = "inbound" if np.median(diffs[0:extrema[0]]) < 0 else "outbound"
+    orbits: list[Trajectory] = [Trajectory(0, int(extrema[0]), in_out_bound_label)]
+
+    for i in range(1, len(extrema)):
+
+        # print(diffs[extrema[i - 1]:extrema[i]])
+        in_out_bound_label = "inbound" if np.median(diffs[extrema[i - 1]:extrema[i]]) < 0 else "outbound"
+
+        orbits.append(
+            Trajectory(extrema[i - 1] + 1, extrema[i], in_out_bound_label)
+        )
+
+    in_out_bound_label = "inbound" if np.median(diffs[extrema[-1]:]) < 0 else "outbound"
+    orbits.append(Trajectory(extrema[-1] + 1, len(distance) - 1, in_out_bound_label))
+
+    return orbits
+
+def identify_orbits(
+        self: RBMDataSet | RBMNcDataSet,
+        orbit_type: Literal["R", "L*"] = "R",
+        minimal_distance: int = 10,
+        *,
+        apply_smoothing: bool = True) -> list[Trajectory]:
+
+    dist = self.R0 if orbit_type == "R" else self.Lstar[:, -1]
+
+    return _identify_orbits(self.datetime, dist, minimal_distance, apply_smoothing=apply_smoothing)
