5b2b99a78c0b0fc114660a6dfc9913f2716376b6
Sahil Jhawar <jhawar@gfz.de>
2026-02-10 14:35:44 +0100

    test: update data directory structure and improve file existence checks in SymhOMNI tests

--- tests/io/symh/test_symh_omni.py
+++ tests/io/symh/test_symh_omni.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
+import shutil
 from datetime import datetime, timezone
 from pathlib import Path
 
@@ -12,7 +13,7 @@ import pytest
 from swvo.io.symh import SymhOMNI
 
 TEST_DIR = os.path.dirname(__file__)
-DATA_DIR = Path(os.path.join(TEST_DIR, "../omni/data/"))
+DATA_DIR = Path(os.path.join(TEST_DIR, "../omni/data/OMNI"))
 
 
 class TestSymhOMNI:
@@ -53,8 +54,8 @@ class TestSymhOMNI:
         end_time = datetime(2020, 12, 31, tzinfo=timezone.utc)
         # download this file without mocking
         symhomni.download_and_process(start_time, end_time)
-
-        assert (DATA_DIR / "OMNI_HIGH_RES_1min_2020.csv").exists()
+        for file in (DATA_DIR / "OMNI" / "2020").glob("OMNI_HIGH_RES_1min_2020*.csv"):
+            assert file.exists()
 
     def test_read_without_download(self, symhomni):
         start_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
@@ -64,25 +65,17 @@ class TestSymhOMNI:
         ):  # value error is raised when no files are found hence no concatenation is possible
             symhomni.read(start_time, end_time, download=False)
 
-    def test_read_with_download(self, symhomni, mock_symhomni_data, mocker):
-        mocker.patch("pathlib.Path.exists", return_value=False)
-        mocker.patch.object(symhomni, "_read_single_file", return_value=mock_symhomni_data)
-        mocker.patch.object(symhomni, "download_and_process")
-
-        start_time = datetime(2020, 1, 1)
-        end_time = datetime(2020, 12, 31)
-
+    def test_read_with_download(self, symhomni):
+        start_time = datetime(2021, 12, 1, tzinfo=timezone.utc)
+        end_time = datetime(2022, 1, 31, tzinfo=timezone.utc)
         df = symhomni.read(start_time, end_time, download=True)
-        symhomni.download_and_process.assert_called_once()
 
-        assert not df.empty
-        assert all(df["sym-h"] == -15.0)
+        assert isinstance(df, pd.DataFrame)
+        assert len(df) > 0
         assert "sym-h" in df.columns
-        assert all(idx.tzinfo is not None for idx in df.index)
-        assert all(idx.tzinfo is timezone.utc for idx in df.index)
 
     def test_read_single_file(self, symhomni):
-        csv_file = Path(DATA_DIR) / "OMNI_HIGH_RES_1min_2020.csv"
+        csv_file = Path(DATA_DIR) / "2012/OMNI_HIGH_RES_1min_201212.csv"
         df = symhomni._read_single_file(csv_file)
         assert isinstance(df, pd.DataFrame)
         assert len(df) > 0
@@ -98,4 +91,5 @@ class TestSymhOMNI:
         assert result_df.index.max() == pd.Timestamp("2013-01-01 00:00:00+00:00")
 
     def test_remove_processed_file(self):
-        os.remove(Path(DATA_DIR) / "OMNI_HIGH_RES_1min_2020.csv")
+        shutil.rmtree(Path(DATA_DIR) / "2021", ignore_errors=True)
+        shutil.rmtree(Path(DATA_DIR) / "2022", ignore_errors=True)
