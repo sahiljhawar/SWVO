277755055d59548cb65329c4bf3f49ddafedecd8
Sahil Jhawar <jhawar@gfz.de>
2026-02-12 17:35:35 +0100

    chore: logging in daily downloads

--- run_downloads.sh
+++ run_downloads.sh
@@ -4,4 +4,4 @@
 
 source /PAGER/FLAG/code/external_data/SWVO/.venv/bin/activate
 source /PAGER/FLAG/set_env_variables.sh
-python3 /PAGER/FLAG/code/external_data/SWVO/download_daily_files.py --logs /PAGER/FLAG/logs/daily_downloads_external_data/
+python3 /PAGER/FLAG/code/external_data/SWVO/download_daily_files.py
