c8adfe6ea65fd5b82354801800db671a78e1c726
Sahil Jhawar <jhawar@gfz.de>
2026-03-26 13:22:20 +0100

    chore: add license header

--- tests/io/kp/test_kp_bgs.py
+++ tests/io/kp/test_kp_bgs.py
@@ -1,4 +1,5 @@
-# SPDX-FileCopyrightText: 2025 GFZ Helmholtz Centre for Geosciences
+# SPDX-FileCopyrightText: 2026 GFZ Helmholtz Centre for Geosciences
+# SPDX-FileContributor: Sahil Jhawar
 #
 # SPDX-License-Identifier: Apache-2.0
 
